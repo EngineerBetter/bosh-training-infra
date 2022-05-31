54d8807f9813f16d76a6fee97bdbd6c41a4ce89a
Tom Godkin <tom.godkin@engineerbetter.com>
2022-05-31 11:57:06 +0100

    Configure security

--- terraform/network.tf
+++ terraform/network.tf
@@ -10,3 +10,59 @@ resource "aws_subnet" "public" {
   availability_zone       = "eu-west-2a"
   map_public_ip_on_launch = true
 }
+
+resource "aws_security_group" "bosh" {
+  name   = "bosh"
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_security_group_rule" "ssh" {
+  security_group_id = aws_security_group.bosh.id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = [
+    "212.140.222.159/32"
+  ]
+}
+
+resource "aws_security_group_rule" "agent" {
+  security_group_id = aws_security_group.bosh.id
+  type              = "ingress"
+  from_port         = 6868
+  to_port           = 6868
+  protocol          = "tcp"
+  cidr_blocks = [
+    "212.140.222.159/32"
+  ]
+}
+
+resource "aws_security_group_rule" "director" {
+  security_group_id = aws_security_group.bosh.id
+  type              = "ingress"
+  from_port         = 25555
+  to_port           = 25555
+  protocol          = "tcp"
+  cidr_blocks = [
+    "212.140.222.159/32"
+  ]
+}
+
+resource "aws_security_group_rule" "internal_tcp" {
+  security_group_id = aws_security_group.bosh.id
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  self              = true
+}
+
+resource "aws_security_group_rule" "internal_udp" {
+  security_group_id = aws_security_group.bosh.id
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "udp"
+  self              = true
+}
