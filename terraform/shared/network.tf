7a1c830e8d9a00201f070e9f03f6ddd64858cb27
Tom Godkin <tom.godkin@engineerbetter.com>
2022-06-29 15:37:44 +0100

    Fix terraform type

--- terraform/shared/network.tf
+++ terraform/shared/network.tf
@@ -90,7 +90,7 @@ resource "aws_route_table" "subnet-route-table" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.bastion
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.bastion.id
   }
 }
