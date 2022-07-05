5b696950724f37e91f0a29cd451ad437828d971b
William Young <will.young@engineerbetter.com>
2022-07-05 11:43:00 +0100

    Create set up for bastion-key & proxy for ssh
    Signed-off-by: William Young <will.young@engineerbetter.com>
    Co-authored-by: Cai Cooper <cai.cooper@engineerbetter.com>
    Co-authored-by: Marcus Dantas <marcus.dantas@engineerbetter.com>

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BASTION_KEY_FILE=bastion-key.pem
+BASTION_KEY_LOCATION=$HOME/.ssh/$BASTION_KEY_FILE
+
+mkdir -p $HOME/.ssh
+
+if [ ! -f $BASTION_KEY_LOCATION ]
+then
+    mv ./$BASTION_KEY_FILE $BASTION_KEY_LOCATION
+fi
+
+eval "$(cat .env)"
+
+if [ ! $(pgrep ssh) ]
+then
+    ssh -4 -D 12345 -fNC $BASTION_USER@$BASTION_HOST -i $BASTION_KEY_LOCATION
+fi
