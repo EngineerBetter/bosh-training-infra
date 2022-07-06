0b02a9ea78be672b92fbc5218713c16ef5c10859
Cai Cooper <cagiti@users.noreply.github.com>
2022-07-06 17:49:24 +0100

    Only create tunnels when they don't exist
    Co-authored-by: William Young <will.young@engineerbetter.com>
    Co-authored-by: Marcus Dantas <marcus.dantas@engineerbetter.com>

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CF_SOCKS5_PORT=12345
-BOSH_SOCKS5_PORT=12346
+CF_SOCKS5_PORT="12345"
+BOSH_SOCKS5_PORT="12346"
 BASTION_KEY_FILE=bastion-key.pem
 BASTION_KEY_LOCATION=$HOME/.ssh/$BASTION_KEY_FILE
 BOSH_USER=vcap
@@ -15,7 +15,13 @@ fi
 
 eval "$(cat .env)"
 
-pkill ssh
-ssh -4 -D $CF_SOCKS5_PORT -fNC $BASTION_USER@$BASTION_HOST -i $BASTION_KEY_LOCATION >/dev/null 2>/dev/null &
+if  [[ $(ps -ef | grep -c "[s]sh -4 -D $CF_SOCKS5_PORT") -eq 0 ]]
+then
+    ssh -4 -D $CF_SOCKS5_PORT -fNC -o StrictHostKeyChecking="no" $BASTION_USER@$BASTION_HOST -i $BASTION_KEY_LOCATION >/dev/null 2>/dev/null &
+fi
+
 sleep 2
-ssh -4 -D $BOSH_SOCKS5_PORT -fNC -i $BASTION_KEY_LOCATION -o ProxyCommand="nc -X 5 -x 127.0.0.1:${CF_SOCKS5_PORT} %h %p" vcap@$BOSH_ENVIRONMENT >/dev/null 2>/dev/null &
+if [[ $(ps -ef | grep -c "[s]sh -4 -D $CF_SOCKS5_PORT") -eq 1 ]] && [[ $(ps -ef | grep -c "[s]sh -4 -D $BOSH_SOCKS5_PORT") -eq 0 ]]
+then
+	ssh -4 -D $BOSH_SOCKS5_PORT -fNC -i $BASTION_KEY_LOCATION -o StrictHostKeyChecking="no" -o ProxyCommand="nc -X 5 -x 127.0.0.1:${CF_SOCKS5_PORT} %h %p" vcap@$BOSH_ENVIRONMENT >/dev/null 2>/dev/null &
+fi
