902b473889f9e0ecc7ed70efd5deeb1303dc1def
Cai Cooper <cagiti@users.noreply.github.com>
2022-07-06 07:46:39 +0100

    Add further proxy command for BOSH SSH

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+CF_SOCKS5_PORT=12345
+BOSH_SOCKS5_PORT=12346
 BASTION_KEY_FILE=bastion-key.pem
 BASTION_KEY_LOCATION=$HOME/.ssh/$BASTION_KEY_FILE
+BOSH_USER=vcap
 
 mkdir -p $HOME/.ssh
 
@@ -12,7 +15,7 @@ fi
 
 eval "$(cat .env)"
 
-if [ ! $(pgrep ssh) ]
-then
-    ssh -4 -D 12345 -fNC $BASTION_USER@$BASTION_HOST -i $BASTION_KEY_LOCATION &
-fi
+pkill ssh
+ssh -4 -D $CF_SOCKS5_PORT -fNC $BASTION_USER@$BASTION_HOST -i $BASTION_KEY_LOCATION >/dev/null 2>/dev/null &
+sleep 2
+ssh -4 -D $BOSH_SOCKS5_PORT -fNC -i $BASTION_KEY_LOCATION -o ProxyCommand="nc -X 5 -x 127.0.0.1:${CF_SOCKS5_PORT} %h %p" vcap@$BOSH_ENVIRONMENT >/dev/null 2>/dev/null &
