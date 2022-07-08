4a77c9580c15a232d6cae564bf21c45f01026254
Cai Cooper <cagiti@users.noreply.github.com>
2022-07-08 19:39:05 +0100

    Ensure public IPs are mapped on instance launch

--- terraform/students/network.tf
+++ terraform/students/network.tf
@@ -62,7 +62,7 @@ resource "aws_subnet" "students" {
   vpc_id                  = data.aws_vpc.main.id
   cidr_block              = each.value
   availability_zone       = var.availability_zone
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
   tags = {
     Name = "bosh-training-${each.key}"
   }
