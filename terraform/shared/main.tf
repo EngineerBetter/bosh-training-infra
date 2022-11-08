54bdd26fb0eaf63d87dc53b1081572076d0ba21e
Tom Godkin <tom.godkin@engineerbetter.com>
2022-11-08 13:34:30 +0000

    Fix tflint issues in shared terraform

--- terraform/shared/main.tf
+++ terraform/shared/main.tf
@@ -11,4 +11,13 @@ provider "aws" {
 
 terraform {
   backend "s3" {}
+
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source  = "registry.terraform.io/hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
--- terraform/shared/variables.tf
+++ terraform/shared/variables.tf
@@ -1,15 +1,28 @@
-variable "region" {}
+variable "region" {
+  type = string
+}
 
 variable "vpc_cidr" {
+  type    = string
   default = "10.0.0.0/16"
 }
 
-variable "key_name" {}
+variable "key_name" {
+  type = string
+}
 
-variable "public_key" {}
+variable "public_key" {
+  type = string
+}
 
-variable "cs_office_ip" {}
+variable "cs_office_ip" {
+  type = string
+}
 
-variable "availability_zone" {}
+variable "availability_zone" {
+  type = string
+}
 
-variable "eb_ci_nat_gateway" {}
+variable "eb_ci_nat_gateway" {
+  type = string
+}
