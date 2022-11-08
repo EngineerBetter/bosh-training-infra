3d5650b7c89fb4146cbcbdea84f3829df8822804
Tom Godkin <tom.godkin@engineerbetter.com>
2022-11-08 11:47:27 +0000

    Revert "Create student specific SGs"
    This reverts commit 26055542340302fdf08bb0506997706b912b26db.

--- terraform/students/network.tf
+++ terraform/students/network.tf
@@ -7,30 +7,18 @@ data "aws_security_group" "main" {
 }
 
 locals {
-  students = { for student in var.students : student.name => toset([for ip in student.ips : "${ip}/32"]) }
   student_ips  = toset(flatten([for student in var.students : [for ip in student.ips : "${ip}/32"]]))
-}
-
-resource "aws_security_group" "students" {
-  for_each = local.students
-
-  name   = each.key
-  vpc_id = var.vpc_id
-
-  tags = { "Name" = "bosh-${each.key}" }
+  director_ips = [for student in var.students : "${cidrhost(student.subnet_cidr, 6)}/32"]
 }
 
 resource "aws_security_group_rule" "ingress_ssh" {
-  for_each = local.students
-
-  security_group_id = aws_security_group.students[each.key]
+  security_group_id = var.security_group_id
   type              = "ingress"
   protocol          = "tcp"
   from_port         = 22
   to_port           = 22
-  cidr_blocks       = each.value
-
-  tags = { "Name" = "ingress_ssh" }
+  cidr_blocks       = local.student_ips
+  count             = length(local.student_ips) == 0 ? 0 : 1
 }
 
 resource "aws_security_group_rule" "ingress_cf_api" {
