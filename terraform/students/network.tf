1aa73a815435e2aaf25883e6c9a25fc5d8080e10
Tom Godkin <tom.godkin@engineerbetter.com>
2022-11-08 14:15:06 +0000

    Fix broken map for student cidr blocks

--- terraform/students/network.tf
+++ terraform/students/network.tf
@@ -3,13 +3,13 @@ data "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "students" {
-  for_each                = var.students
+  for_each                = { for student in var.students : student.name => student.subnet_cidr }
   vpc_id                  = data.aws_vpc.main.id
-  cidr_block              = each.subnet_cidr
+  cidr_block              = each.value
   availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
   tags = {
-    Name = "bosh-training-${each.name}"
+    Name = "bosh-training-${each.key}"
   }
 }
 
